a3d2729fabe6fe896ee5a0d93759b2b28d91dd1f
Tristan Davey <tristan.davey@safetyculture.com.au>
2014-01-09 17:40:14 +1000

    Integrate Changes from typeahead.js Pull Request #215
    Changes from commit
    https://github.com/willtcarey/typeahead.js/blob/bd6d080d69895d6c062df8f7
    8e4f1c4efc0b76f3/src/dataset.js

--- dist/typeahead.js
+++ dist/typeahead.js
@@ -394,6 +394,7 @@
             this.itemHash = {};
             this.adjacencyList = {};
             this.storage = o.name ? new PersistentStorage(o.name) : null;
+            this.allowDuplicates = o.allowDuplicates || false;
         }
         utils.mixin(Dataset.prototype, {
             _processLocalData: function(data) {
@@ -534,7 +535,7 @@
                     suggestions = suggestions.slice(0);
                     utils.each(data, function(i, datum) {
                         var item = that._transformDatum(datum), isDuplicate;
-                        isDuplicate = utils.some(suggestions, function(suggestion) {
+                        isDuplicate = that.allowDuplicates ? false : utils.some(suggestions, function(suggestion) {
                             return item.value === suggestion.value;
                         });
                         !isDuplicate && suggestions.push(item);
